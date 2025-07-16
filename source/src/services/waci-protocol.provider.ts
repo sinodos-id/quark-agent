0120160bab3176e23ea061b633c6b4d70a6a6c65
scammi <scammi@gmail.com>
2025-07-16 15:58:19 -0300

    return on missing ivntitation

--- source/src/services/waci-protocol.provider.ts
+++ source/src/services/waci-protocol.provider.ts
@@ -38,6 +38,7 @@ export const WACIProtocolProvider: Provider = {
               invitationId: waciInvitationId,
               holderId,
             });
+            return;
           }
 
           const {
